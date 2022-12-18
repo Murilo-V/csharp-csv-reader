0526eaea5de7d36031fd40683ac97fa6f7a419e1
Murilo-V <alveiramurilo@hotmail.com>
2022-12-18 17:45:32 -0300

    feat: csv to object

---
+++ csv_reader/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using CsvHelper;
+
+using var streamReader = File.OpenText("/Users/alveira/Projects/csv_reader/csv_reader/username.csv");
+using var csvReader = new CsvReader(streamReader, CultureInfo.CurrentCulture);
+
+var users = csvReader.GetRecords<User>();
+
+foreach (var user in users)
+{
+    Console.WriteLine(user);
+}
+
+record User(string Username, string Id, string FirstName, string LastName);
